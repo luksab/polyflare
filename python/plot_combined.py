361cf5fbbce55360647efbf95d168485fbb79afa
luksab <lukas@sabatschus.de>
2022-03-23 20:04:28 +0100

    add colorbars to combined

--- python/plot_combined.py
+++ python/plot_combined.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 
 N=316
+N=190
 def sample(x):
     return int(np.floor((x+1)/2*N))
 
@@ -21,9 +22,34 @@ def plot_dot(ax, name, picture = 0):
 
     ax.set_title("data")
     if picture == 0:
-        ax.imshow(picture1, cmap=mpl.cm.gray)#, interpolation='nearest')
+        img = ax.imshow(picture1, cmap=mpl.cm.gray)#, interpolation='nearest')
+        plt.colorbar(img, ax=ax)
     else:
-        ax.imshow(picture2, cmap=mpl.cm.gray)
+        img = ax.imshow(picture2, cmap=mpl.cm.gray)
+        plt.colorbar(img, ax=ax)
+
+def plot_poly_error(ax, name, picture = 0):
+    data = np.loadtxt(f"dots/dots,{name}.csv", delimiter=",")
+    data_poly = np.loadtxt(f"dots/poly,{name},{picture}.csv", delimiter=",")
+    # data = data - data_poly
+
+    picture1=np.empty((N,N))
+    picture1[:,:]=np.nan
+    picture2=np.empty((N,N))
+    picture2[:,:]=np.nan
+    for (row, poly) in zip(data, data_poly):
+        ix=sample(row[0])
+        iy=sample(row[1])
+        picture1[ix,iy]=row[2] - poly[2]
+        picture2[ix,iy]=row[3] - poly[2]
+
+    ax.set_title("sparse error")
+    if picture == 0:
+        img = ax.imshow(picture1, cmap=mpl.cm.gray)#, interpolation='nearest')
+        plt.colorbar(img, ax=ax)
+    else:
+        img = ax.imshow(picture2, cmap=mpl.cm.gray)
+        plt.colorbar(img, ax=ax)
 
 def plot_poly(ax, name, num = 0):
     data = np.loadtxt(f"dots/poly,{name},{num}.csv", delimiter=",")
@@ -36,7 +62,8 @@ def plot_poly(ax, name, num = 0):
         picture[ix,iy]=row[2]
 
     ax.set_title("sparse poly")
-    ax.imshow(picture, cmap=mpl.cm.gray)
+    img = ax.imshow(picture, cmap=mpl.cm.gray)
+    plt.colorbar(img, ax=ax)
 
 def plot_de_poly(ax, name, num = 0):
     data = np.loadtxt(f"dots/depoly,{name},{num}.csv", delimiter=",")
@@ -49,7 +76,8 @@ def plot_de_poly(ax, name, num = 0):
         picture[ix,iy]=row[2]
 
     ax.set_title("dense poly")
-    ax.imshow(picture, cmap=mpl.cm.gray)
+    img = ax.imshow(picture, cmap=mpl.cm.gray)
+    plt.colorbar(img, ax=ax)
 
 def plot_combined(name, num = 0):
     fig, axs = plt.subplots(2,2)
@@ -57,7 +85,9 @@ def plot_combined(name, num = 0):
     plot_dot(axs[0, 0], name, num)
     plot_poly(axs[0, 1], name, num)
     plot_de_poly(axs[1,0], name, num)
+    plot_poly_error(axs[1,1], name, num)
     plt.tight_layout()
+    # plt.show()
     fig.savefig(f"dots/combinded,{name},{num}.png")
 
 for i in range(1,6):
